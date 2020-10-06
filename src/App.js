bad40e3e307c40f85a83589e9c03e33ba9ae5e62
Arshansh <arshanshagarwal1@gmail.com>
2020-10-06 18:14:11 +0530

    broken our homepage into smaller component, finishing styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import HomePage from "./homepage.component";
-import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import "./app.css";
 
 function App() {
   return (
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => <Directory />;
+
+export default HomePage;
